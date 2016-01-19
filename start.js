f43452a0da97ef02d5af94e1fcf6cd2a8cc5de14
gaudi <craig@resin.io>
2016-01-19 15:47:45 +0000

    a

--- start.js
+++ start.js
@@ -1,73 +1,70 @@
-var config = require('./env.json');
-var fs = require('fs');
-var async = require("async");
-var exec = require('child_process').exec;
-var child;
-
-var parentDir =__dirname;
-
-cloneApps()
-
-function cloneApps() {
-  async.each(config.APPS,
-    function(app, callback){
-      try {
-        // check if the app exists
-        fs.accessSync(parentDir + "/.." + "/" + app.name, fs.F_OK);
-        callback();
-      } catch (e) {
-        child = exec("cd /" + parentDir + "/.. && git clone " + app.repo + " " + app.name + "&& cd " + app.name + " && git remote add resin " + config.REMOTE, function (error, stdout, stderr) {
-          console.log(stdout);
-          console.log(stderr);
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-          callback();
-        });
-      }
+// var config = require('./env.json');
+// var fs = require('fs');
+// var async = require("async");
+// var exec = require('child_process').exec;
+// var Git = require("nodegit");
+// var path = require('path');
+// var child;
+//
+// var parentDir =__dirname;
+//
+// var deployFolderName = 'test';
+//
+var cloneOptions = {};
+cloneOptions.remoteCallbacks = {
+    credentials: function(url, userName) {
+        return NodeGit.Cred.sshKeyFromAgent(userName);
     },
-    // 3rd param is the function to call when everything's done
-    function(err){
-      // All tasks are done now
-      checkRemotes();
+    transferProgress: function(info) {
+      return console.log(info);
     }
-  );
-}
+};
+//
+// cloneApps()
+//
+// function cloneApps() {
+//   async.each(config.APPS,
+//     function(app, callback){
+//
+//       repoPath = path.join(deployFolderName, app.name)
+//       console.log(repoPath)
+//       try {
+//         // check if the app exists
+//         fs.accessSync(repoPath, fs.F_OK);
+//         callback();
+//       } catch (e) {
+//         console.log("cloning")
+//         Git.Clone(app.repo, repoPath, cloneOptions)
+//
+//         // Look up this known commit.
+//         .then(function(repo) {
+//           // Use a known commit sha from this repository.
+//           console.log(repo)
+//         }).catch(function(err) { console.log(err); });
+//       }
+//     },
+//     // 3rd param is the function to call when everything's done
+//     function(err){
+//       // All tasks are done now
+//       console.log("start")
+//     }
+//   );
+// }
+//
+// function startApp() {
+//   child = exec("npm start", function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (error !== null) {
+//       console.log('exec error: ' + error);
+//     } else {
+//       console.log('starting app');
+//     }
+//   });
+// }
+var Git = require("nodegit");
 
-function checkRemotes() {
-  async.each(config.APPS,
-    function(app, callback){
-      try {
-        // check if the app exists
-        fs.accessSync(parentDir + "/.." + "/" + app.name, fs.F_OK);
-        callback();
-      } catch (e) {
-        child = exec("cd /" + parentDir + "/.." + "/" + app.name + " && git remote set-url resin " + config.REMOTE, function (error, stdout, stderr) {
-          console.log(stdout);
-          console.log(stderr);
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-          callback();
-        });
-      }
-    },
-    // 3rd param is the function to call when everything's done
-    function(err){
-      // All tasks are done now
-      startApp();
-    }
-  );
-}
-
-function startApp() {
-  child = exec("npm start", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    } else {
-      console.log('starting app');
-    }
-  });
-}
+Git.Clone("https://github.com/nodegit/nodegit", "flip", cloneOptions).then(function(repository) {
+  // Work with the repository object here.
+  return repository;
+}).catch(function(err) { console.log(err); });
