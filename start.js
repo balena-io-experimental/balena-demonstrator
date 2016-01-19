d33557e844026d4dc8805477f7f96e2fe20ca88a
gaudi <craig@resin.io>
2016-01-19 14:01:39 +0000

    reset REMOTE with every commit

--- start.js
+++ start.js
@@ -6,21 +6,9 @@ var child;
 
 var parentDir =__dirname;
 
-check()
+cloneApps()
 
-function startApp() {
-  child = exec("npm start", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    } else {
-      console.log('starting app');
-    }
-  });
-}
-
-function check() {
+function cloneApps() {
   async.each(config.APPS,
     function(app, callback){
       try {
@@ -41,7 +29,45 @@ function check() {
     // 3rd param is the function to call when everything's done
     function(err){
       // All tasks are done now
+      checkRemotes();
+    }
+  );
+}
+
+function checkRemotes() {
+  async.each(config.APPS,
+    function(app, callback){
+      try {
+        // check if the app exists
+        fs.accessSync(parentDir + "/.." + "/" + app.name, fs.F_OK);
+        callback();
+      } catch (e) {
+        child = exec("cd /" + parentDir + "/.." + "/" + app.name + " && git remote set-url resin " + config.REMOTE, function (error, stdout, stderr) {
+          console.log(stdout);
+          console.log(stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          callback();
+        });
+      }
+    },
+    // 3rd param is the function to call when everything's done
+    function(err){
+      // All tasks are done now
       startApp();
     }
   );
+}
+
+function startApp() {
+  child = exec("npm start", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    } else {
+      console.log('starting app');
+    }
+  });
 }
