0b83caff08335cae130965a9bd058568a6b6ac60
gaudi <craig@resin.io>
2016-01-20 08:56:44 +0000

    nodegit for init script

--- start.js
+++ start.js
@@ -1,15 +1,14 @@
-// var config = require('./env.json');
-// var fs = require('fs');
-// var async = require("async");
-// var exec = require('child_process').exec;
-// var Git = require("nodegit");
-// var path = require('path');
-// var child;
-//
-// var parentDir =__dirname;
-//
-// var deployFolderName = 'test';
-//
+var config = require('./env.json');
+var fs = require('fs');
+var async = require("async");
+var exec = require('child_process').exec;
+var Git = require("nodegit");
+var path = require('path');
+var child;
+
+var parentDir =__dirname;
+
+
 var cloneOptions = {};
 cloneOptions.remoteCallbacks = {
     credentials: function(url, userName) {
@@ -19,52 +18,55 @@ cloneOptions.remoteCallbacks = {
       return console.log(info);
     }
 };
-//
-// cloneApps()
-//
-// function cloneApps() {
-//   async.each(config.APPS,
-//     function(app, callback){
-//
-//       repoPath = path.join(deployFolderName, app.name)
-//       console.log(repoPath)
-//       try {
-//         // check if the app exists
-//         fs.accessSync(repoPath, fs.F_OK);
-//         callback();
-//       } catch (e) {
-//         console.log("cloning")
-//         Git.Clone(app.repo, repoPath, cloneOptions)
-//
-//         // Look up this known commit.
-//         .then(function(repo) {
-//           // Use a known commit sha from this repository.
-//           console.log(repo)
-//         }).catch(function(err) { console.log(err); });
-//       }
-//     },
-//     // 3rd param is the function to call when everything's done
-//     function(err){
-//       // All tasks are done now
-//       console.log("start")
-//     }
-//   );
-// }
-//
-// function startApp() {
-//   child = exec("npm start", function (error, stdout, stderr) {
-//     console.log('stdout: ' + stdout);
-//     console.log('stderr: ' + stderr);
-//     if (error !== null) {
-//       console.log('exec error: ' + error);
-//     } else {
-//       console.log('starting app');
-//     }
-//   });
-// }
-var Git = require("nodegit");
 
-Git.Clone("https://github.com/nodegit/nodegit", "flip", cloneOptions).then(function(repository) {
-  // Work with the repository object here.
-  return repository;
-}).catch(function(err) { console.log(err); });
+function setUp() {
+  async.each(config.APPS,
+    function(app, callback){
+      try {
+        // check if the app exists
+        fs.accessSync(app.name, fs.F_OK);
+        callback();
+      } catch (e) {
+        console.log("cloning")
+        Git.Clone(app.repo, path.join('apps', app.name), cloneOptions).then(function(repo) {
+          // Work with the repository object here.
+          Git.Remote.setUrl(repo, 'resin', config.REMOTE);
+        }).catch(function(err) {
+          console.log(err);
+          Git.Repository.open(path.join('apps', app.name)).then(function(repo) {
+            console.log(repo)
+            Git.Remote.lookup(repo, 'resin', callback).then(function(remote) {
+              if(remote) {
+                Git.Remote.setUrl(repo, 'resin', config.REMOTE);
+              } else {
+                Git.Remote.create(repo, 'resin', config.REMOTE);
+              }
+            });
+          }).catch(function(err) {
+            console.log(err);
+          });
+        });
+      }
+    },
+    // 3rd param is the function to call when everything's done
+    function(err){
+      // All tasks are done now
+      console.log("start");
+      startApp()
+    }
+  );
+}
+
+function startApp() {
+  child = exec("npm start", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    } else {
+      console.log('starting app');
+    }
+  });
+}
+
+setUp()
